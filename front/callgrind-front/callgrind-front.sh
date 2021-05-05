b59b832d34b6def9b4d8c25e229b567a579c9d80
nefanov <nefanov90@gmail.com>
2021-05-05 10:51:14 +0300

    gprof2dot (3rd party) is added

--- front/callgrind-front/callgrind-front.sh
+++ front/callgrind-front/callgrind-front.sh
@@ -2,5 +2,10 @@
 
 # usual pipeline example for gprof:
 # gprof ./main | gprof2dot -s | dot -Tpng -o output.png
+# this script usage example:
+# ./callgrind-front.sh tests/callgrind.out.2322 result.dot
+# produces result.dot and result.dot.png in current working directory
+
 pwd
 python3 ../../third-party/gprof2dot/gprof2dot.py ${1} --format=callgrind -c print -o ${2}
+dot ${2} -Tpng -o ${2}.png
