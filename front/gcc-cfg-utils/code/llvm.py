e8ace6cf23145f8697842a892ad9fb335ebaf6f7
pr0kuk <alex-shch@yandex.ru>
2021-07-03 14:46:47 +0300

    added interface to core for llvm

--- front/gcc-cfg-utils/code/llvm.py
+++ front/gcc-cfg-utils/code/llvm.py
@@ -1,21 +1,43 @@
 import sys
-data = open(sys.argv[1]).readlines()
-l1, l2, rules, d1 = [], [], [], {}
-for i in data:
-    a, b, c = i.find("["), i.find("->"), i.find(":")
-    if a != -1:
-        d1.update([(i[1:a-1], i[i.find("{")+1:-4])])
-        l1.append(i[1:a-1])
-    if b != -1:
-        if c < b and c != -1:
-            b = c + 1
-        l2.append((i[1:b-1], i[i.find("->") + 3:-2]))
-n = int(input())
+import subprocess
+import os
 graph = open("data/graph_llvm", "w")
-graph.write("1\n" + str(n)+"\n")
-for i in range(n):
-    rules.append(raw_input().split())
-    graph.write(rules[i][0] + " " + rules[i][1] + "\n")
-graph.write("\n" + str(len(l2)) + " " + str(len(l2)) + "\n")
-for i in range(len(l2)):
-    graph.write(str(l1.index(l2[i][0])) + " " + str(l1.index(l2[i][1])) + " " + raw_input(str(l2[i])) + " " + "\n")
+graph.write(str(len(sys.argv) - 1) + "\n") #print number of functions
+
+for file in range(1, len(sys.argv)):
+    if file != 1:
+        graph.write("\n")
+    print("Analyzing file " + sys.argv[file])
+    data = open(sys.argv[file]).readlines()
+    nodes, edges, rules, blocks_of_code = [], [], [], {}
+
+    for i in data:
+        a, b, c = i.find("["), i.find("->"), i.find(":")
+        if a != -1:
+            blocks_of_code.update([(i[1:a-1], i[i.find("{")+1:-4])])
+            nodes.append(i[1:a-1])
+        if b != -1:
+            if c < b and c != -1:
+                b = c + 1
+            edges.append((i[1:b-1], i[i.find("->") + 3:-2]))
+
+    n = int(input("Specify the number of rules: "))
+    graph.write(str(n) + "\n")
+
+    #input rules
+    for i in range(n):
+        rules.append(raw_input().split())
+        graph.write(rules[i][0] + " " + rules[i][1] + "\n")
+    
+    #output
+    graph.write("\n" + str(len(edges)) + " " + str(len(edges)) + "\n") #write number of edges
+    for i in nodes:
+        graph.write(i + " ")
+    graph.write("\n")
+    for i in range(len(edges)):
+        graph.write(str(nodes.index(edges[i][0])) + " " + str(nodes.index(edges[i][1])) + " " + raw_input(str(edges[i])) + " " + "\n")
+
+#proc = subprocess.call(["ls", "-la"])
+path_to = os.path.abspath(sys.argv[0])[:os.path.abspath(sys.argv[0]).find("code")]
+print(path_to)
+proc = subprocess.call([path_to + "build/core", path_to + "build/core", path_to + "build"])
