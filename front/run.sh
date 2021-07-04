b174d6fa895b4312cab16a1eaae2dd4fb4dc1443
pr0kuk <alex-shch@yandex.ru>
2021-07-04 16:01:18 +0300

    edited .sh scripts

--- front/run.sh
+++ front/run.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
-if [[ $0 == "run.sh" ]]; then
-    path="./"
-else
-    path=${0%run.sh}
-fi
+path=${0/run.sh/"./"}
+path=${path/start.sh/"../"}
 if [[ $1 == "-test" ]]; then
-    if [[ $0 == "run.sh" ]]; then
-        build/test
-    else
-        cd $path
-        build/test
-    fi
+    cd $path
+    build/test
+else
+    $path/build/code2graph $@
 fi
