47c42a40f06a344bc9d09483f70eab2758dea3ae
fassstem <fedorov.ar@phystech.edu>
2021-07-14 21:41:33 +0300

    First version of thin-bb released

--- front/gcc-cfg-utils/code/c2g.cpp
+++ front/gcc-cfg-utils/code/c2g.cpp
@@ -42,7 +42,6 @@ void entry_end_handler(int& subgraph, int& k, std::string& inp, int len, int& ba
 	if (inp[len - 1] == '\\')
 	{
 		basic_block = 1;
-		for(k; k< len && inp[k] != '"'; k++);
 		code = "";
 	}
 	else
@@ -164,23 +163,37 @@ void adjacency_list(std::vector <std::vector<std::pair<std::string, int>>>& V, s
 
 void new_graph_creator(std::vector<std::vector<std::pair<std::string, int>>>& V, std::vector<std::vector<std::vector<std::pair<int, std::string>>>>& E, std::vector<std::vector<std::string>>& Code, std::vector<std::vector<int>>& V_new, std::vector<std::vector<std::pair<int, std::string>>>& E_new, std::vector<std::string>& Code_new)
 {
-	for (int i = 0; i < V.size(); i++)
-	{
-		for (int j = 0; j < V[i].size(); j++)
-		{			
+	for (int i = 0; i < V.size(); i++) {
+		for (int j = 0; j < V[i].size(); j++) {
 			V[i][j].second = V_new.size();
-			V_new.push_back({i, j, 0});
-			Code_new.push_back(Code[i][j]);
+			std::string code = Code[i][j];
+			int found = -1, counter = 0;
+			while (found != code.size() - 1) {
+				code = code.substr(found + 1, code.size() - found - 1);
+				if (code.substr(0, 4) == "|if\\") {
+					Code_new.push_back(code);
+					found = code.size() - 1;
+				}
+				else {
+					found = code.find('\n');
+					Code_new.push_back(code.substr(0, found + 1));
+				}
+				V_new.push_back({i, j, counter});
+				E_new.resize(V_new.size());
+				if (counter != 0) {
+					E_new[E_new.size() - 2].push_back(std::make_pair(E_new.size() - 1, ""));
+				}
+				counter++;
+			}
 		}
 	}
-	E_new.resize(V_new.size());
-	for (int i = 0; i < E.size(); i++)
-	{
-		for (int j = 0; j < E[i].size(); j++)
-		{
-			for (int q = 0; q < E[i][j].size(); q++)
-			{
-				E_new[V[i][j].second].push_back(std::make_pair(V[i][E[i][j][q].first].second, E[i][j][q].second));
+	for (int i = 0; i < E.size(); i++) {
+		for (int j = 0; j < E[i].size(); j++) {
+			for (int q = 0; q < E[i][j].size(); q++) {
+				int s = V[i][j].second, f = V[i][E[i][j][q].first].second;
+				while (s + 1 < V_new.size() && V_new[s + 1][2] != 0)
+					s++;
+				E_new[s].push_back(std::make_pair(f, ""));
 			}
 		}
 	}
@@ -188,56 +201,95 @@ void new_graph_creator(std::vector<std::vector<std::pair<std::string, int>>>& V,
 
 void graph_merger(std::vector <std::pair<std::string, std::pair<int, int>>>& Clusters, std::vector<std::vector<std::pair<std::string, int>>>& V, std::vector<std::vector<std::vector<std::pair<int, std::string>>>>& E, std::vector<std::vector<std::string>>& Code, std::vector<std::vector<int>>& V_new, std::vector<std::vector<std::pair<int, std::string>>>& E_new, std::vector<std::string>& Code_new)
 {
-	for (int i = 0; i < V_new.size(); i++)
-	{
-		int foundmin = Code_new[i].size(), cl_min = -1;
-		for (int j = 0; j < Clusters.size(); j++)
-		{
-			int FLAG = 0, found = 0, found1 = 0;
-			while (1)
-			{
-				std::string func_name = "\\ " + Clusters[j].first + "\\";
-				found = Code_new[i].find(func_name, found1);
-				if (found != std::string::npos)
-				{
-					int counter = 0;
-					for (int q = 0; q < found; q++)
-						if (Code_new[i][q] == '"')
-							counter++;
-					if (counter % 2 == 0)
-					{
-						FLAG = 1;
-						break;
-					}
-					found1 = Code_new[i].find('\n', found);
-				}
-				else
-					break;
+	for (int i = 0; i < V_new.size(); i++) {
+		for (int j = 0; j < Clusters.size(); j++) {
+			std::string func_name = "\\ " + Clusters[j].first + "\\";
+			int found = Code_new[i].find(func_name);
+			if (found != std::string::npos) {
+				E_new[V[j][Clusters[j].second.second].second].insert(E_new[V[j][Clusters[j].second.second].second].end(), E_new[i].begin(), E_new[i].end());
+				E_new[i].clear();
+				E_new[i].push_back(std::make_pair(V[j][Clusters[j].second.first].second, ""));
 			}
-			if (FLAG == 1 && found < foundmin)
-			{
-				foundmin = found;
-				cl_min = j;
-			}
-		}		
-		if (cl_min != -1)
-		{
-			int found1 = Code_new[i].find('\n', foundmin);
-			if (found1 != Code_new[i].size() - 1) {
-				std::string code = Code_new[i].substr(found1 + 1, Code_new[i].size() - found1 - 1);
-				Code_new[i] = Code_new[i].substr(0, found1 + 1);
-				V_new.push_back({V_new[i][0], V_new[i][1], V_new[i][2] + 1});
-				Code_new.push_back(code);
-				E_new.push_back(E_new[i]);
-				E_new[V[cl_min][Clusters[cl_min].second.second].second].push_back(std::make_pair(E_new.size() - 1, ""));
-			}
-			else 
-				E_new[V[cl_min][Clusters[cl_min].second.second].second].insert(E_new[V[cl_min][Clusters[cl_min].second.second].second].end(), E_new[i].begin(), E_new[i].end());
-			E_new[i].clear();
-			E_new[i].push_back(std::make_pair(V[cl_min][Clusters[cl_min].second.first].second, ""));
 		}
 	}
 }
+
+// void new_graph_creator(std::vector<std::vector<std::pair<std::string, int>>>& V, std::vector<std::vector<std::vector<std::pair<int, std::string>>>>& E, std::vector<std::vector<std::string>>& Code, std::vector<std::vector<int>>& V_new, std::vector<std::vector<std::pair<int, std::string>>>& E_new, std::vector<std::string>& Code_new)
+// {
+// 	for (int i = 0; i < V.size(); i++)
+// 	{
+// 		for (int j = 0; j < V[i].size(); j++)
+// 		{			
+// 			V[i][j].second = V_new.size();
+// 			V_new.push_back({i, j, 0});
+// 			Code_new.push_back(Code[i][j]);
+// 		}
+// 	}
+// 	E_new.resize(V_new.size());
+// 	for (int i = 0; i < E.size(); i++)
+// 	{
+// 		for (int j = 0; j < E[i].size(); j++)
+// 		{
+// 			for (int q = 0; q < E[i][j].size(); q++)
+// 			{
+// 				E_new[V[i][j].second].push_back(std::make_pair(V[i][E[i][j][q].first].second, E[i][j][q].second));
+// 			}
+// 		}
+// 	}
+// }
+
+// void graph_merger(std::vector <std::pair<std::string, std::pair<int, int>>>& Clusters, std::vector<std::vector<std::pair<std::string, int>>>& V, std::vector<std::vector<std::vector<std::pair<int, std::string>>>>& E, std::vector<std::vector<std::string>>& Code, std::vector<std::vector<int>>& V_new, std::vector<std::vector<std::pair<int, std::string>>>& E_new, std::vector<std::string>& Code_new)
+// {
+// 	for (int i = 0; i < V_new.size(); i++)
+// 	{
+// 		int foundmin = Code_new[i].size(), cl_min = -1;
+// 		for (int j = 0; j < Clusters.size(); j++)
+// 		{
+// 			int FLAG = 0, found = 0, found1 = 0;
+// 			while (1)
+// 			{
+// 				std::string func_name = "\\ " + Clusters[j].first + "\\";
+// 				found = Code_new[i].find(func_name, found1);
+// 				if (found != std::string::npos)
+// 				{
+// 					int counter = 0;
+// 					for (int q = 0; q < found; q++)
+// 						if (Code_new[i][q] == '"')
+// 							counter++;
+// 					if (counter % 2 == 0)
+// 					{
+// 						FLAG = 1;
+// 						break;
+// 					}
+// 					found1 = Code_new[i].find('\n', found);
+// 				}
+// 				else
+// 					break;
+// 			}
+// 			if (FLAG == 1 && found < foundmin)
+// 			{
+// 				foundmin = found;
+// 				cl_min = j;
+// 			}
+// 		}		
+// 		if (cl_min != -1)
+// 		{
+// 			int found1 = Code_new[i].find('\n', foundmin);
+// 			if (found1 != Code_new[i].size() - 1) {
+// 				std::string code = Code_new[i].substr(found1 + 1, Code_new[i].size() - found1 - 1);
+// 				Code_new[i] = Code_new[i].substr(0, found1 + 1);
+// 				V_new.push_back({V_new[i][0], V_new[i][1], V_new[i][2] + 1});
+// 				Code_new.push_back(code);
+// 				E_new.push_back(E_new[i]);
+// 				E_new[V[cl_min][Clusters[cl_min].second.second].second].push_back(std::make_pair(E_new.size() - 1, ""));
+// 			}
+// 			else 
+// 				E_new[V[cl_min][Clusters[cl_min].second.second].second].insert(E_new[V[cl_min][Clusters[cl_min].second.second].second].end(), E_new[i].begin(), E_new[i].end());
+// 			E_new[i].clear();
+// 			E_new[i].push_back(std::make_pair(V[cl_min][Clusters[cl_min].second.first].second, ""));
+// 		}
+// 	}
+// }
 
 
 void new_graph_list(std::vector <std::pair<std::string, std::pair<int, int>>>& Clusters, std::vector <std::vector<std::pair<std::string, int>>>& V)
---
+++ front/gcc-cfg-utils/code/wo-thin-bb.cpp
@@ -0,0 +1,78 @@
+#include "code2graph.h"
+
+void new_graph_creator(std::vector<std::vector<std::pair<std::string, int>>>& V, std::vector<std::vector<std::vector<std::pair<int, std::string>>>>& E, std::vector<std::vector<std::string>>& Code, std::vector<std::vector<int>>& V_new, std::vector<std::vector<std::pair<int, std::string>>>& E_new, std::vector<std::string>& Code_new)
+{
+	for (int i = 0; i < V.size(); i++)
+	{
+		for (int j = 0; j < V[i].size(); j++)
+		{			
+			V[i][j].second = V_new.size();
+			V_new.push_back({i, j, 0});
+			Code_new.push_back(Code[i][j]);
+		}
+	}
+	E_new.resize(V_new.size());
+	for (int i = 0; i < E.size(); i++)
+	{
+		for (int j = 0; j < E[i].size(); j++)
+		{
+			for (int q = 0; q < E[i][j].size(); q++)
+			{
+				E_new[V[i][j].second].push_back(std::make_pair(V[i][E[i][j][q].first].second, E[i][j][q].second));
+			}
+		}
+	}
+}
+
+void graph_merger(std::vector <std::pair<std::string, std::pair<int, int>>>& Clusters, std::vector<std::vector<std::pair<std::string, int>>>& V, std::vector<std::vector<std::vector<std::pair<int, std::string>>>>& E, std::vector<std::vector<std::string>>& Code, std::vector<std::vector<int>>& V_new, std::vector<std::vector<std::pair<int, std::string>>>& E_new, std::vector<std::string>& Code_new)
+{
+	for (int i = 0; i < V_new.size(); i++)
+	{
+		int foundmin = Code_new[i].size(), cl_min = -1;
+		for (int j = 0; j < Clusters.size(); j++)
+		{
+			int FLAG = 0, found = 0, found1 = 0;
+			while (1)
+			{
+				std::string func_name = "\\ " + Clusters[j].first + "\\";
+				found = Code_new[i].find(func_name, found1);
+				if (found != std::string::npos)
+				{
+					int counter = 0;
+					for (int q = 0; q < found; q++)
+						if (Code_new[i][q] == '"')
+							counter++;
+					if (counter % 2 == 0)
+					{
+						FLAG = 1;
+						break;
+					}
+					found1 = Code_new[i].find('\n', found);
+				}
+				else
+					break;
+			}
+			if (FLAG == 1 && found < foundmin)
+			{
+				foundmin = found;
+				cl_min = j;
+			}
+		}		
+		if (cl_min != -1)
+		{
+			int found1 = Code_new[i].find('\n', foundmin);
+			if (found1 != Code_new[i].size() - 1) {
+				std::string code = Code_new[i].substr(found1 + 1, Code_new[i].size() - found1 - 1);
+				Code_new[i] = Code_new[i].substr(0, found1 + 1);
+				V_new.push_back({V_new[i][0], V_new[i][1], V_new[i][2] + 1});
+				Code_new.push_back(code);
+				E_new.push_back(E_new[i]);
+				E_new[V[cl_min][Clusters[cl_min].second.second].second].push_back(std::make_pair(E_new.size() - 1, ""));
+			}
+			else 
+				E_new[V[cl_min][Clusters[cl_min].second.second].second].insert(E_new[V[cl_min][Clusters[cl_min].second.second].second].end(), E_new[i].begin(), E_new[i].end());
+			E_new[i].clear();
+			E_new[i].push_back(std::make_pair(V[cl_min][Clusters[cl_min].second.first].second, ""));
+		}
+	}
+}
