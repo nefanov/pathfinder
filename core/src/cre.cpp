06d82f23f558add678d21c351259abad83e7179d
pr0kuk <alex-shch@yandex.ru>
2021-07-17 17:50:38 +0300

    fixed bug with empty words

--- core/src/cre.cpp
+++ core/src/cre.cpp
@@ -28,10 +28,11 @@ int input_rules(int& initial, std::vector <rule>& eps_rules, std::vector <std::v
         if (FLAG == nonterminals.size())
             nonterminals.push_back(left);
         if (right != "0")
-            lol[right[0] - 'a'].push_back(FLAG); //if rules can be raped..
+            lol[right[0] - 'a' + 1].push_back(FLAG); //if rules can be raped..
         else
             lol[0].push_back(FLAG), eps_rules.push_back({0, FLAG, {0, 0}, right});
         rules.push_back({0, FLAG, {0, 0}, right});
+
     } 
     else {
         std::string a_2, a_3;//, a_2(right[0]), a_3(right[1]);
@@ -52,9 +53,9 @@ int input_rules(int& initial, std::vector <rule>& eps_rules, std::vector <std::v
 
 int filling_loops(int j, int P, rule& i, std::deque <std::vector<int>>& W, std::vector <std::vector <std::vector <unsigned int> > >& H1v, std::vector <std::vector <std::vector <unsigned int> > >& H2v, std::vector <std::vector <std::unordered_set <int> > >& H1u, std::vector <std::vector <std::unordered_set <int> > >& H2u, void (*add_value)(std::unordered_set<int>& u, std::vector<unsigned int>& v, int a, int P))
 {
-    W.push_back({i.left, j, i.left});
-    add_value(H1u[j][i.left], H1v[j][i.left], i.left, P);
-    add_value(H2u[j][i.left], H2v[j][i.left], i.left, P);
+    W.push_back({j, i.left, j});
+    add_value(H1u[i.left][j], H1v[i.left][j], j, P);
+    add_value(H2u[i.left][j], H2v[i.left][j], j, P);
     return 0;
 }
 
@@ -64,7 +65,7 @@ int filling_edge_matrices(int P, std::ifstream& fin, std::vector <std::vector<in
     std::string str;
     fin >> u1 >> u2 >> str;
     if (str != "0")
-        s = str[0] - 'a';
+        s = str[0] - 'a' + 1;
     for (auto j: lol[s]) {
         W.push_back({u1, j, u2});
         add_value(H1u[j][u1], H1v[j][u1], u2, P);
