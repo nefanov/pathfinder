b3f57a4d5fe641ed09ded7f4ddd13327a3274b40
nefanov <efanov@phystech.edu>
2021-09-28 16:17:53 +0300

    Manual grammar input is optional now
    Checking EOF from input for omitting manual grammar input if needed.

--- front/gcc-cfg-utils/code/code2graph.cpp
+++ front/gcc-cfg-utils/code/code2graph.cpp
@@ -30,25 +30,30 @@ int main(int argc, char* argv[])
 	}  
 	void* ngc = dlsym(sl, "new_graph_creator");
 	if (ngc == NULL) {
-        fprintf(stderr, "%s\n", dlerror());
-        return -1;
-    }
+        	fprintf(stderr, "%s\n", dlerror());
+        	return -1;
+    	}
 	void* gm = dlsym(sl, "graph_merger");
 	if (gm == NULL) {
-        fprintf(stderr, "%s\n", dlerror());
-        return -1;
-    }
+        	fprintf(stderr, "%s\n", dlerror());
+        	return -1;
+    	}
 	funcs_front func(ngc, gm);
 	func.new_graph_creator(V, E, Code, V_new, E_new, Code_new);
 	func.graph_merger(Clusters, V, E, Code, V_new, E_new, Code_new);
 	new_graph_list(Clusters, V);
 	new_vertex_list(V, Clusters, V_new, Code_new);
 	new_adjacency_list(E_new);
-	std::vector<std::pair<std::string, std::string> > rules;
-	input_V_E(input_file, E_new, rules, analyze_file);
-	visualising_graph(opt.path_to_save_file, V_new, E_new, Code_new, V);
-	to_fifo(bin_path, V_new, V, E_new, rules);
-	if (!opt.is_front_only)
-  		execl((bin_path + "core").c_str(), (bin_path + "core").c_str(), "data/graph", (opt.is_fast) ? "-fast" : "-slow", NULL);
+	int inp_check = input_file.peek();
+        if (inp_check != EOF) {
+                std::vector<std::pair<std::string, std::string> > rules;
+                input_V_E(input_file, E_new, rules, analyze_file);
+                visualising_graph(opt.path_to_save_file, V_new, E_new, Code_new, V);
+                to_fifo(bin_path, V_new, V, E_new, rules);
+                if (!opt.is_front_only)
+                        execl((bin_path + "core").c_str(), (bin_path + "core").c_str(), "data/graph", (opt.is_fast) ? "-fast" : "-slow", NULL);
+        } else {
+                visualising_graph(opt.path_to_save_file, V_new, E_new, Code_new, V);
+        }
 	return 0;
 }
