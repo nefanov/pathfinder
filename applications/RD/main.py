08d1c843c2817f79559bb6073a23f1a89866c1d0
Nikolay Efanov <nefanov90@gmail.com>
2021-09-29 15:06:32 -0400

    experiment from article: labeling is added

--- applications/RD/main.py
+++ applications/RD/main.py
@@ -10,7 +10,10 @@ sys.path.append(os.path.join(current_path, "../front/preprocessor/lexer"))
 
 # API imports
 import lexer
+from predicates import *
 
+# specificators imports
+from mem_spec import *
 
 def prepare_graph_example():
     """
@@ -33,5 +36,26 @@ def prepare_graph_example():
                         specializer=None)
 
 
+
+def prepare_graph_from_article():
+    #print("TODO: move the code here")
+    os.chdir("../../front")
+    os.system('./get_thin_graph.sh -i 1.c -s m.dot -p pic25.png')
+    wdir = os.path.join(current_path, "../front")
+    in_graph = os.path.join(wdir, 'm.dot')
+    out_f = os.path.join(wdir, 'processed.dot')
+    out_pic = os.path.join(wdir, 'processed_pic.png')
+    os.chdir(current_path)
+    print(in_graph, out_f, out_pic, current_path, os.getcwd())
+    lexer.prepare_graph(in_graph, out_f, out_pic,
+                        need_graph_save=True,
+                        need_plot=True,
+                        pattern_composer=compose_mem_pattern,
+                        specializer=specialize_Dflow)
+
+
 if __name__ == '__main__':
-    prepare_graph_example()
+    if (len(sys.argv)<2 or (sys.argv[1] =="--test" and sys.argv[2]=="empty_labeling")):
+        prepare_graph_example()
+    elif (sys.argv[1]=="--test" and sys.argv[2]=="malloc_memset"):
+       prepare_graph_from_article()
--- applications/RD/mem_spec.py
+++ applications/RD/mem_spec.py
@@ -1,13 +1,28 @@
-import graphlex as glex
-from predicates import *
+import os
+import sys
+
+# API paths
+current_path = os.path.dirname(os.path.abspath("."))
+sys.path.append(os.path.join(current_path, "../front/preprocessor/lexer"))
+
+# API imports
+import lexer
+
+
+def shortest_path_check(graph, src, dst):
+    nx_graph = nx.nx_pydot.from_pydot(graph)
+    if nx.shortest_path(nx_graph, src, dst):
+        return True
+    return False
+
 
 def compose_mem_pattern(scenario={}):
     if not scenario:  # if scenario is not preset
         P = {'yes_df_list': [], 'no_df_list': [], 'yes_cf_list': [], 'no_cf_list': [], 'rel_kinds': set()}
         # first for good paths
-        rel = glex.Relation(left={'type': "assign_function_call"},
+        rel = lexer.glex.Relation(left={'type': "assign_function_call"},
                        right={'type': "assign_function_call"},
-                       predicate=predicate_malloc_memset,
+                       predicate=lexer.predicate_malloc_memset,
                        extra=None,
                        label="DF_malloc_memset",
                        params={"edge_style": {"color": "dodgerblue"}})
@@ -17,16 +32,16 @@ def compose_mem_pattern(scenario={}):
             ["assign_function_call any", "any assign_function_call", "assign_function_call assign_function_call"])
 
         # then for forbridden paths
-        no_rel = glex.Relation(left={'type': "assign_function_call"},
+        no_rel = lexer.glex.Relation(left={'type': "assign_function_call"},
                           right={'type': "assign_aryphmetic_op"},
-                          predicate=predicate_malloc_getptr,
+                          predicate=lexer.predicate_malloc_getptr,
                           extra=None,
                           label="DF_malloc_getptr",
                           params={"edge_style": {"color": "dodgerblue"}})
 
-        no_rel_next = glex.Relation(left={'type': "assign_aryphmetic_op"},
+        no_rel_next = lexer.glex.Relation(left={'type': "assign_aryphmetic_op"},
                                right={'type': "assign_const"},
-                               predicate=predicate_getptr_write,
+                               predicate=lexer.predicate_getptr_write,
                                extra=None,
                                label="DF_malloc_getptr_writemem",
                                params={"edge_style": {"color": "#f76d23"}})
@@ -37,9 +52,9 @@ def compose_mem_pattern(scenario={}):
     else:
         pass  # TO DO: support scenarios
     return P
-  
-  
-  def specialize_Dflow(graph, nodes, node_lex_dict, P):
+
+
+def specialize_Dflow(graph, nodes, node_lex_dict, P):
     """
     # nodes: словарь по типам вершин
     # P -- list(patterns (relation ))
@@ -52,8 +67,8 @@ def compose_mem_pattern(scenario={}):
                 # TO DO: check scope!
                 if not p.extra and p.predicate(n1s['format'], n2s['format']) and shortest_path_check(graph, n, n2):
                     graph.add_edge(pydot.Edge(n,
-                                    n2,
-                                    color=p.params["edge_style"]["color"],
-                                    style ='dashed',
-                                    label=p.label))
+                                   n2,
+                                   color=p.params["edge_style"]["color"],
+                                   style ='dashed',
+                                   label=p.label))
     return graph
