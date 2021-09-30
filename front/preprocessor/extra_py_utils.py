7c6519aba80f4d1fd86a6579d19eae7f492a57a4
nefanov <nikolay.efanov@huawei.com>
2021-09-30 15:40:08 +0300

    func def parsing stuff is moved to extra_py_utils; example is added

--- front/preprocessor/extra_py_utils.py
+++ front/preprocessor/extra_py_utils.py
@@ -1,4 +1,29 @@
 import re
+import lexer
+import os, sys
+
+def get_function_args(arg_str):
+    spl = arg_str.split(",")
+    return spl
+
+
+def parse_function_call_args(item):
+    if item['exp'] == assign_function_call:
+        arg_list = get_function_args(item['format']['arguments'])
+        return arg_list
+    return None
+
+
+def parse_function_def_args(st):
+    arg_list = get_function_args(st)
+    if len(arg_list[0]) == 0:
+        return arg_list
+    return [pos.split()[-1][1:]
+            if pos.split()[-1][0] == '*'
+            else pos.split()[-1]
+            for pos in arg_list] # maybe should be rewritten -- pointer matters...
+
+#===================================================================================
 
 pattern=r'^([a-zA-Z_*$][a-zA-Z_*$0-9]*\s+)+([a-zA-Z_*$][a-zA-Z_*$0-9]*)\s*\((.*)\)'
 
@@ -16,12 +41,15 @@ def extract_func_def_list(fname):
 		lines = f.readlines()
 		res = search_pattern(pattern, lines, fname)
 		return res
-
+#===================================================================================
 
 if __name__ == '__main__':
 	"""
 	test
 	"""
-	import sys
 	import pprint
-	pprint.pprint(extract_func_def_list(sys.argv[1]))
+	fl = extract_func_def_list(sys.argv[1])	
+	arg_names = [parse_function_def_args(it[-1]) for it in [el[-1] for el in [item[-1] for item in fl]]]
+	func_names =[parse_function_def_args(it[-2]) for it in [el[-1] for el in [item[-1] for item in fl]]]
+	pprint.pprint(arg_names)
+	print(func_names)
