0a15bb09a4afd9211cfaf6d6a23489c97365d4e5
nefanov <efanov@phystech.edu>
2021-10-12 00:17:09 +0300

    Update grammar.py

--- front/pstree-front/grammar.py
+++ front/pstree-front/grammar.py
@@ -10,9 +10,7 @@ def GetKey(val, d):
 
    return "key doesn't exist"
 
-def construct(W=["aaaaa"], n=2):
-
-	
+def construct(W=["aaaaab"], n=2):
 	num_cnt = 0
 	grammar_table = {}
 	l_name = ""
@@ -28,7 +26,6 @@ def construct(W=["aaaaa"], n=2):
 				grammar_table.update({l_name:right})
 			
 			chunks.append(l_name)
-	print("chunks 1:", chunks)
 	# compose rules Nj --> NiNk
 	while (len(chunks)>1):
 		chunks = [list(chunks[i:i + n])
@@ -37,13 +34,19 @@ def construct(W=["aaaaa"], n=2):
 		if (len(chunks[-1])== 0):
 			chunks = chunks[:-1]
 		new_chunks = []
+
+
 		for right in chunks:
-			if right in grammar_table.values():
-				l_name = GetKey(right, grammar_table)
+			if right in grammar_table.values(): # X ::== right
+				l_name = GetKey(right, grammar_table) # lname <- X
 			else:
-				num_cnt += 1
-				l_name = "N"+str(num_cnt)
-				grammar_table.update({l_name:right})
+				if (len(right)==2):
+					num_cnt += 1
+					l_name = "N"+str(num_cnt)
+					grammar_table.update({l_name:right})
+				else:
+					l_name = right[0]
+				
 			new_chunks.append(l_name)
 
 		chunks = new_chunks
@@ -53,10 +56,8 @@ def construct(W=["aaaaa"], n=2):
 	l_name = "S"
 	grammar_table.update({l_name:chunks[0]})
 
-
 	# remove chain rules
 	k = GetKey(grammar_table[chunks[0]], grammar_table)
-	print(grammar_table[k],";;;",k)
 	grammar_table.update({l_name: grammar_table[k]})
 	del grammar_table[k]
 	print(grammar_table)
