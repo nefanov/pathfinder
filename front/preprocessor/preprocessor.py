655ff325863b3b4d11493d045b156c55626dce68
nefanov <efanov@phystech.edu>
2022-05-08 10:36:07 +0300

    preprocessor refactoring

--- front/preprocessor/preprocessor.py
+++ front/preprocessor/preprocessor.py
@@ -5,15 +5,12 @@ from networkx.generators.social import les_miserables_graph
 import pydot
 
 import extra_py_utils
-from extra_py_utils import *
+import mem_spec
+import lexer.lexer as lexer
 
 current_path = os.path.dirname(os.path.abspath("."))
 sys.path.append(os.path.join(current_path, "preprocessor"))
 sys.path.append(os.path.join(current_path, (".." + os.sep)*2 + "third-party"))
-
-import mem_spec
-import lexer.lexer as lexer
-
 
 def prepare_custom_markup(scenario=None,
                           spec=mem_spec.specialize_prev_mem_dep,
@@ -44,23 +41,23 @@ def prepare_custom_markup(scenario=None,
         FRONT_CMD_SUFFIX = "-SSA"
 
     os.system("gcc-9 " + GCC_CMD + " " + src + " && sync " + src +".*t.*.dot")
-    os.system("echo "+src+" | "+front_dir+"/build/code2graph -cmd "+src+" -front-only -thin " + FRONT_CMD_SUFFIX +" > " +src + "_gcc_frontend.output")
+    os.system("echo "+src+" | "+front_dir+"/build/code2graph -cmd "+src+" -front-only -thin " + FRONT_CMD_SUFFIX +
+              " > " +src + "_gcc_frontend.output")
     os.system("sync "+src+"_gcc_frontend.output")
-    os.system("csplit " + src + "_gcc_frontend.output " +"'/digraph /'" + " > /dev/null")
+    os.system("csplit " + src + "_gcc_frontend.output " + "'/digraph /'" + " > /dev/null")
     os.system("rm "+src+"_gcc_frontend.output && sync")
-    os.system("cat xx01 > " + ir_dotfile + " && sync "+ ir_dotfile)
+    os.system("cat xx01 > " + ir_dotfile + " && sync " + ir_dotfile)
     os.system("rm xx*")
     os.system("sed -i '$ d' " + ir_dotfile +" && sync "+ ir_dotfile)
-    os.system("rm "+ src +".*t.*")
-    os.system("echo Picture generating from dotfile " + ir_dotfile + ", saving to: " +ir_pic)
+    os.system("rm " + src + ".*t.*")
+    os.system("echo Picture generating from dotfile " + ir_dotfile + ", saving to: " + ir_pic)
 
-    #os.system('./get_thin_graph.sh -i ' + os.path.join(wdir, src) + ' -s '+ os.path.join(wdir, ir_dotfile) + ' -p '+os.path.join(wdir,ir_pic))
     os.system("dot -Tpng " + ir_dotfile + " -o " + ir_pic)
     # os.system('mv ' + 'front' + os.sep + src + '.\\* '+ wdir)
     in_graph = os.path.join(wdir, ir_dotfile)
     if need_interproc_pass:
         g = pydot.graph_from_dot_file(in_graph)
-        ft  = extra_py_utils.func_table(os.path.join(wdir, src)) #"../front/1.c")
+        ft  = extra_py_utils.func_table(os.path.join(wdir, src))
         g[0] = extra_py_utils.prepare_interproc_graph_var_trans(g[0], ft)
         g[0].write_raw(in_graph)
 
