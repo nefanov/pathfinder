c6f610e51dd4948da7cbce06b1d70ddb1bcd55d6
LednevaAR <ledneva.ar@phystech.edu>
2022-08-12 16:21:55 +0300

    iguanatry21

--- core/src/core.cpp
+++ core/src/core.cpp
@@ -6,9 +6,9 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include<iostream>
+#include <iostream>
 #include <fstream> 
-#include<limits.h>
+#include <limits.h>
 using namespace std;
 
 int main(int argc, char* argv[]) {
